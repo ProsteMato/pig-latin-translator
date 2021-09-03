983cc89785c067dd8b0f3be2fc77553b73275a80
Martin Koci <mkoci11@gmail.com>
2021-09-04 01:49:15 +0200

    Add documentation and complete implementation of translator.

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * This file is implementing an english to pig latin translator. Pig latin translator supports translation of word
+ * and sentences. It also supports different dialects.
+ *
+ * @author Martin Koči <mkoci11@gmail.com>
+ */
 
 declare(strict_types=1);
 
@@ -13,14 +19,23 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
     private $consonant = "";
     private $vowel = "";
     private $other = "";
+    private $notWord = "";
 
-    function __construct()
+    public function __construct()
     {
-        $this->consonant = '/^([^aeiouy]+)(.*)/';
-        $this->vowel = '/^([aeiouy]+)(.*)/';
-        $this->other = '/^((qu)+)(.*)/';
+        parent::__construct();
+        $this->consonant = '/^([^aeiouy]+)(.*)/i';
+        $this->vowel = '/^([aeiouy]+)(.*)/i';
+        $this->other = '/^((qu)+)(.*)/i';
+        $this->notWord = '/([^[:alnum:]]+)/i';
     }
 
+    /**
+     * @brief When this action is called it will return translated string from english to pig latin as JSON response.
+     * @param string $value English string
+     * @param string $dialect Chosen dialect which will be used in translation.
+     * @throws Nette\Application\AbortException
+     */
     public function actionTranslate(string $value, string $dialect): void
     {
         $this->sendResponse(new JsonResponse([
@@ -28,20 +43,59 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
         ]));
     }
 
+    /**
+     * @brief This function translates all words one by one from english to pig latin.
+     * @param string $value English string.
+     * @param string $dialect Chosen dialect.
+     * @return string Translated string to pig latin.
+     */
     private function translate(string $value, string $dialect): string
     {
         $translated_string = "";
-        $words_to_translate = explode(' ', $value);
+
+        //split string into words
+        $words_to_translate = preg_split($this->notWord, $value, 0, PREG_SPLIT_DELIM_CAPTURE);
 
         foreach ($words_to_translate as $word) {
-            if (preg_match($this->vowel, $value)) {
-                $translated_string .= preg_replace($this->vowel, "$1$2" . $dialect, $word);
-            } elseif (preg_match($this->other, $value)) {
-                $translated_string .= preg_replace($this->other, "$3-$1" . $dialect, $word);
-            } elseif (preg_match($this->consonant, $value)) {
-                $translated_string .= preg_replace($this->consonant, "$2-$1ay", $word);
+            $capitalized = false;
+
+            //Check if first char of english word is upper.
+            if (!empty($word) && ctype_upper($word[0])) {
+                $capitalized = true;
+                $word = lcfirst($word);
             }
 
+            //translate word to pig latin
+            $translated_word = $this->translateWord($word, $dialect);
+
+            //If english word started with upper letter convert first character to upper.
+            if ($capitalized) {
+                $translated_string .= ucfirst($translated_word);
+                $capitalized = false;
+            } else {
+                $translated_string .= $translated_word;
+            }
+        }
+        return $translated_string;
+    }
+
+    /**
+     * @brief This function translates one word from english to pig latin.
+     * @param string $word English word.
+     * @param string $dialect Chosen dialect.
+     * @return string Translated word.
+     */
+    private function translateWord(string $word, string $dialect): string
+    {
+        $translated_string = "";
+        if (preg_match($this->notWord, $word)) { // no alphanumerical characters
+            $translated_string = $word;
+        } elseif (preg_match($this->vowel, $word)) { // begins with vowels
+            $translated_string = preg_replace($this->vowel, "$1$2-" . $dialect, $word);
+        } elseif (preg_match($this->other, $word)) { // begins with silent consonant
+            $translated_string = preg_replace($this->other, "$3-$1" . $dialect, $word);
+        } elseif (preg_match($this->consonant, $word)) { // begins with consonant or cluster of consonants
+            $translated_string = preg_replace($this->consonant, "$2-$1ay", $word);
         }
         return $translated_string;
     }
